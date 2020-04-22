4f2cf9834970e3b092d18ea3e21b02711898f819
fangchunyu <fangchunyu@hust.edu.cn>
2020-04-22 21:51:39 +0800

    Delete test_gupconv.m

--- @csConv/test_gupconv.m
+++
@@ -1,7 +0,0 @@
-clc;
-clear all;
-
-brain_struct=load('brain25_100.mat');
-brain=brain_struct.test;
-psf=genpsf(4,1000);
-result= GPUConv3D( single(brain), int32(size(brain)), single(psf), int32(size(psf)));
